8030bd0e0bc67f72e9d14c905f1cfbd06174f519
sollendy <edlannino@tutanota.com>
2023-03-09 16:44:38 +0100

    inizio codifica in JS

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//creo l'array di oggetti comprendente titoli, descrizioni e immagini
+//dopo aver cancellato gli elementi statici lo uso per popolare dinamicamente la parte dell'immagine
+//dopo ciò creo una variabile indice che mi tenga il conto 
+//creo l'evento click in cui dico che al clic le immagini devono scorrere a seconda di quale freccia venga cliccata
